2b4c6f314f337e4517472317cdc692b7639e6737
jamesyang <295183327@qq.com>
2023-09-08 10:03:24 +0800

    结合 Swagger UI 该项目已撒花完结

--- cmd/main.go
+++ cmd/main.go
@@ -5,16 +5,11 @@ import (
 	"demo/oapi-codegen-go/app"
 	"github.com/deepmap/oapi-codegen/pkg/middleware"
 	"github.com/labstack/echo/v4"
-	"github.com/labstack/gommon/log"
 	"net/http"
 )
 
-const test = "Get order info failed: %v."
-
 func main() {
 	e := echo.New()
-	e.Debug = true
-	e.Logger.SetLevel(log.DEBUG)
 	server := app.EchoServer{}
 	codegenTest.RegisterHandlersWithBaseURL(e, &server, "/james")
 	// swagger 对象
@@ -22,9 +17,9 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	// 增加参数校验中间件：对 swagger 对象解析，并对请求参数校验
+	// demo 1: 增加参数校验中间件：对 swagger 对象解析，并对请求参数校验
 	// e.Use(middleware.OapiRequestValidator(swagger))
-	// 自定义参数校验
+	// demo 2: 自定义参数校验
 	options := middleware.Options{
 		ErrorHandler: func(c echo.Context, err *echo.HTTPError) error {
 			println(err.Error())
@@ -32,6 +27,20 @@ func main() {
 		},
 	}
 	e.Use(middleware.OapiRequestValidatorWithOptions(swagger, &options))
+	// demo 3: Swagger UI 结合 Echo：在 echo 中配置中间件，对 Swagger 路由特殊预处理
+	e.Pre(func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			if c.Request().URL.Path == "/james/swagger.yaml" {
+				return c.File("./demo.yaml") // 返回 Swagger YAML 文件
+			}
+			if c.Request().URL.Path == "/james/docs" {
+				return c.File("./index.html")
+			}
+			return next(c)
+		}
+	})
+	// 使用 oapi-codegen 校验
+	e.Use(middleware.OapiRequestValidator(swagger))
 	err = e.Start(":8090")
 	if err != nil {
 		panic(err)
